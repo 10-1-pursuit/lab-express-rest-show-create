acab7644364422833738efcf4b160dedeb01d956
xavierrice <xavier.rice@gmail.com>
2023-10-11 13:02:10 -0400

    all bonuses.yas

---
+++ controllers/functions_views.js
@@ -0,0 +1,57 @@
+// const { response } = require("express");
+//const logs_controller = require("./logs_controller");
+
+function AlphabetSort(response, direction) {
+  
+    if (direction === "asc") {
+    const ascSort = response.sort((a, b) =>
+      a.title.localeCompare(b.title, "en", { sensitivity: "case" })
+    );
+    return ascSort;
+  } else if (direction === "desc") {
+    const descSort = response.sort((a, b) =>
+      b.title.localeCompare(a.title, "en", { sensitivity: "case" })
+    );
+    return descSort;
+  } else {
+    return "Error in function";
+  }
+}
+
+function MistakesFilters(response, selection) {
+    let mistakesLogs;
+
+  if (selection === "true"){
+    mistakesLogs = response.filter((log) =>  log.mistakesWereMadeToday);
+  } else if (selection === "false") {
+    mistakesLogs = response.filter((log) =>  !log.mistakesWereMadeToday);
+  } else {
+    return "Error in function";
+  }
+  return mistakesLogs;
+}
+
+function CrisisFilter(response, selection, num){
+    let sortedLastCrisis;
+    
+    if( selection === "gt"){
+         sortedLastCrisis = response.filter((log) => log.daysSinceLastCrisis > num)
+    } else if (selection === "gte"){
+         sortedLastCrisis = response.filter((log) => log.daysSinceLastCrisis >= num)
+    } else if (selection === "lt"){
+        sortedLastCrisis = response.filter((log) => log.daysSinceLastCrisis < num)
+    } else if (selection === "lte"){
+         sortedLastCrisis = response.filter((log) => log.daysSinceLastCrisis <= num)
+    } else {
+        console.log("Error");
+    }
+   return sortedLastCrisis
+}
+         
+
+
+module.exports = {
+  AlphabetSort,
+  MistakesFilters,
+  CrisisFilter
+};
--- controllers/logs_controller.js
+++ controllers/logs_controller.js
@@ -1,105 +1,38 @@
-const test = console.log;
+
 const express = require("express");
 const logs = express.Router();
-const logsArr = require("../models/logs_model");
+const logsArr = require("../models/log");
+const { AlphabetSort, MistakesFilters, CrisisFilter } = require('./functions_views')
 
 logs.get("/", (req, res) => {
 
-  res.json(logsArr);
+  const { order, mistakes, lastCrisis } = req.query
+  const response = logsArr.slice();
 
-  const sortedOrder = req.query.order;
-  const sortedMistakes = req.query.mistakes;
-  const lastCrisis = req.query.lastCrisis;
-
-  const filteredLogs = logsArr.slice();
-
-  if (sortedOrder === "acs") {
-    
-    const ascAlphabetArr = filteredLogs.sort((a, b) => {                        //COULD MOVE TO VIEW
-      for (let i = 0; i < Math.min(a.title.length, b.title.length); i++) {
-        // if one strings is shorter we get undefined. By limiting the loop to the min length we only compare the charters we have
-
-        const charAstrings = a.title.charCodeAt(i); //indexing a string using UTF-16 units to return lone numbers
-        const charBstrings = b.title.charCodeAt(i);
-        if (charAstrings !== charBstrings) {
-          return charAstrings - charBstrings;
-        }
-      }
-      return a.title.length - b.title.length; //shorter words first
-    });
-
-    if (ascAlphabetArr.length === 0){  //checking for empty arr.
-      res.status(420).send("asending array is off");
-    } else {
-      console.log(ascAlphabetArr)
-      res.json(ascAlphabetArr);
-    }
-
-
-  } else if (sortedOrder === "desc") {
-
-    const descAlphabetArr = filteredLogs.sort((a, b) => // I wanted to use localeCompare. //COULD MOVE TO VIEW
-      b.title.localeCompare(a.title, 'en', {sensitivity: 'case'})
-    );
-
-    if (descAlphabetArr.length === 0) {
-      res.status(420).send("decending array is off");
-    } else {
-      console.log(descAlphabetArr)
-      res.json(descAlphabetArr);
-    }
-  } else if (sortedMistakes === true) {
-
-    const mistakesTLogs = filteredLogs.filter((log) => { //COULD MOVE TO VIEW
-      return log.mistakesWereMadeToday;
-    }
-    );
-
-    if (mistakesTLogs.length === 0) {
-      res.status(420).send("error with ur simple filter");
-    } else {
-      console.log(mistakesTLogs);
-      res.json(mistakesTLogs);
-    }
-    
-  } else if (sortedMistakes === false) {
-    
-         const mistakesFLogs = filteredLogs.filter((log) => {     //COULD MOVE TO VIEW
-         return !log.mistakesWereMadeToday;
-         });
-
-
-    if (mistakesFLogs.length === 0) {
-      res.status(420).send("error with ur simple filter");
-    } else {
-      console.log(mistakesFLogs);
-      res.json(mistakesFLogs);
-    }
-
-  } else if(lastCrisis === "gt10"){
-
-    let num = 10
+  if(order){
+   console.log(AlphabetSort(response, order))
+   res.json(AlphabetSort(response, order))
+  }
   
-        const sortedLastCrisis = filteredLogs.filter((log) => {       //COULD MOVE TO VIEW 
-          return log.daysSinceLastCrisis > num;
-         }
-    );
-
-    if (sortedLastCrisis.length === 0){
-        res.status(420).send("Length is off size queen")
-    } else {
-        console.log(sortedLastCrisis);
-        res.json(sortedLastCrisis)
-    }
+  if(mistakes){
+    console.log(MistakesFilters(response, mistakes))
+    res.json(MistakesFilters(response, mistakes))
   }
 
-}
-);
+  if(lastCrisis){
+    console.log(CrisisFilter(response, lastCrisis, 10))
+    res.json(CrisisFilter(response, lastCrisis , 10))
+  }
+  else{
+    res.json(response)
+  }
+});
 
 
 logs.get("/:arrNum", (req, res) => {
   const { arrNum } = req.params;
   if (logsArr[arrNum]) {
+    console.log(logsArr[arrNum])
     res.json(logsArr[arrNum]);
   } else {
     res.status(404).send(`nothing is found on : ${arrNum}`);
