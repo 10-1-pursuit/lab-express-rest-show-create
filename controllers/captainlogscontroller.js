34c50598c314efdae0befab34eed4704b56a8d12
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-10-11 02:06:19 -0400

    lab complete, no bonus

---
+++ controllers/captainlogscontroller.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const captainLogs = express.Router();
+const captianLogsArr = require("../models/log.js");
+
+captainLogs.get("/", (req, res) => {
+    res.send(captianLogsArr)
+});
+
+captainLogs.get("/:id", (req, res) => {
+    const { id } = req.params;
+    if (captianLogsArr[id]) {
+        res.send(captianLogsArr[id]);
+    } else {
+        res.redirect("/404");
+    }
+});
+
+captainLogs.post("/", (req, res) => {
+    const newLog = req.body;
+    captianLogsArr.push(newLog);
+    res.redirect("/")
+});
+
+captainLogs.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    if (captianLogsArr[id]) {
+        captianLogsArr.splice(id, 1);
+        res.redirect("/logs");
+    } else {
+        res.redirect("/404")
+    }
+})
+
+module.exports = captainLogs
