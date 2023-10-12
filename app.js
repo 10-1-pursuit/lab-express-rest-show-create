7f13313aa26ad9e0520c127c58b87f795db9bf45
Natyka Callwood <Natykacallwood@pursuit.org>
2023-10-11 21:37:06 -0400

    Completed lab with all tests passing. Still unable to connect the localhost port in the browser, not sure what is the issue.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const logsController = require('./controllers/logsController.js')
+
+app.use(express.json());
+
+app.use('/logs', logsController);
+
+app.get('*', (req, res) => {
+    res.status(404).json({ error: "Page does not exist!!" });
+  });
+
 
 module.exports = app;
 
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,3 +1,35 @@
-const express = require('express');
-const logs = require("../app.js")
-let logsArray = require("../models/log.js")
+  const express = require('express');
+  const logs = express.Router();
+  const logsArray = require('../models/log.js')
+
+  logs.get('/', (req, res)=>{
+      res.json(logsArray)
+    })
+
+  logs.get("/:myId", (req, res) => {
+      const { myId } = req.params;
+      if (logsArray[myId]) {
+        res.send(logsArray[myId]);
+      } else {
+        res.redirect(logsArray);
+      }
+    });
+    
+  logs.delete("/:myId", (req, res)=>{
+      const { myId } = req.params;
+      const deletedLog = logsArray.splice(req.params.myId, 1);
+      res.status(200).json(deletedLog[0])
+    })
+    
+  logs.put("/:myId", (req, res)=>{
+      const { myId } = req.params;
+      logsArray[myId] = req.body;
+      res.status(200).json(logsArray[myId]);
+    })
+    
+  logs.post("/", (req, res) => {
+        logsArray.push(req.body);
+        res.json(logsArray[logsArray.length - 1])
+      });
+
+  module.exports = logs;
