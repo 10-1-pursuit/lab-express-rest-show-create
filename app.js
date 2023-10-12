9b02151d91e0bcd2aa0df132668eb5f549956c80
Natyka Callwood <Natykacallwood@pursuit.org>
2023-10-11 22:57:58 -0400

    Fixed the issue that was preventing me from connecting to the localhost in the browser. Added a welcome to the captains log message for the '/' route.

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.use(express.json());
 
 app.use('/logs', logsController);
 
+app.get('/', (req, res)=>{
+  res.send("Welcome to the captains's log!!!")
+})
+
 app.get('*', (req, res) => {
     res.status(404).json({ error: "Page does not exist!!" });
   });
