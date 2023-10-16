17d2c27850365dd1d877016f5c3fe22f35acb291
xavierrice <xavier.rice@gmail.com>
2023-10-15 20:47:08 -0400

    adding v2

---
+++ v2/newControllers/newLogsController.js
@@ -0,0 +1,22 @@
+// write some logic to display the index data embedded in an unordered list of anchor tags linking to the show routes at `/v2/logs/:index`
+
+// const displayIndex = (response)=>{
+
+// const ul = document.createElement('ul')
+
+// response.forEach( i => { 
+
+//     const logA = document.createElement('a')
+//     const logLi = document.createElement('li')
+//     const logP = document.createElement('p')
+//     const logH1 = document.createElement('h1')
+//     const logH2 = document.createElement('h2')
+
+//     logA.href = '/v2/${i}'
+//     logP.textContent = i.post
+//     logA.textContent = i.title
+//     logH2.textContent = i.captainName
+
+// });
+
+// }
