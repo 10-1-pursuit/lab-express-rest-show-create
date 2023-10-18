0f66372bd409e5a3090fcb253bff66c5b6e4fc8f
jameel914 <jameelibrahim107@gmail.com>
2023-10-18 16:30:07 -0400

    made some changes

--- app.js
+++ app.js
@@ -3,33 +3,12 @@ const app = express();
 
 app.use(express.json());
 
-const log = require("./models/log.js");
+const logsController = require("./controllers/logsController.js");
+
+app.use("/logs", logsController);
 
 app.get("/", (req, res) => {
   res.send(`Welcome to the captain's log`);
-});
-
-app.get("/logs", (req, res) => {
-  res.send(log);
-});
-
-app.get("/logs/:id", (req, res) => {
-  const { id } = req.params;
-  if (log[id]) {
-    res.json(log[id]);
-  } else {
-    res.redirect("/*");
-  }
-});
-
-app.post("/logs", (req, res) => {
-  log.push(req.body);
-  res.json(log[log.length - 1]);
-});
-
-app.delete("/logs/:id", (req, res) => {
-  const { id } = req.params;
-  res.send(log.splice(id, 1));
 });
 
 app.get("*", (req, res) => {
