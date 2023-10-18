0ee42bb64c6e674d1a151bcd6fdccc62507a6dde
anickacodes <anickalewis@pursuit.org>
2023-10-18 11:44:01 -0400

    troubleshoot module

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const express = require('express')
-const logs = require("./models/log.js");
-const app = express()
+const express = require("express");
+const app = express();
 //parse incoming middleware
-app.use(express.json()) 
+app.use(express.json());
 
+const logsController = require("./v2/controllers/logsController.js");
+
+
+app.use("/logs", logsController);
 
 app.get('/', (req, res) => {
     res.send("Welcome to the Captain's Log, Ayee Matey 🪝⛵️")
 })
 
-app.get('/logs', (req, res) => {
+app.get('/v2/logs', (req, res) => {
     res.send(logs)
 })
 
-app.get('/logs/:arrayIndex', (req, res) => {
+app.get('/v2/logs/:arrayIndex', (req, res) => {
     const {arrayIndex} = req.params
     if(logs[arrayIndex]) {
         res.json(logs[arrayIndex])
     // console.log("excuse", logs[arrayIndex])
-    } else 
+    } else
     res.redirect( 'https://github.com/joinpursuit/captains-log-api' )
 })
 
-app.post('/logs', (req, res) => {
+app.post('/v2/logs', (req, res) => {
     const { captainName,
         title,
         post,
@@ -33,47 +36,40 @@ app.post('/logs', (req, res) => {
     console.log(`sent, ${JSON.stringify(newLog)}`);
     logs.push(newLog)
     res.status(201).json(newLog)
-    
+
 })
 
-app.delete('/logs/:id', (req, res) => {
+app.delete('/v2/logs/:id', (req, res) => {
     const {id} = req.params
     logs.splice(id, 1)
     res.send('Ok')
 })
 
+// const alphaOrder = (req, res, next) => {
+// console.log('✅')
 
+// if (req.body.hasOwnProperty('name')){
+//     return next();
+// } else {
+//     res.send('uh oh')
+// }
 
-const alphaOrder = (req, res, next) => {
-console.log('✅')
+// }
 
+// app.post('/', alphaOrder, (req, res) => {
+//     const sortedLog = Object.keys(logs).sort()
+//     console.log('sent')
+//     logs.push(sortedLog)
+//     res.send('Ok')
+// })
 
-if (req.body.hasOwnProperty('name')){
-    return next();
-} else {
-    res.send('uh oh')
-}
+// app.get('/logs?order=asc', (req, res) => {
 
-}
+// })
 
-app.post('/', alphaOrder, (req, res) => {
-    const sortedLog = Object.keys(logs).sort()
-    console.log('sent')
-    logs.push(sortedLog)
-    res.send('Ok')
-})
-
-
-
-app.get('/logs?order=asc', (req, res) => {
-
-})
-
-
-
-app.all('*', (req, res) => { 
-    res.status(404).send('<h1>404! Page not found</h1>'); 
-  }); 
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>404! Page not found</h1>");
+});
 
 //   app.use((req, res, next) => {
 //     console.log(req.method, req.headers.host, req.path)
@@ -86,4 +82,4 @@ app.all('*', (req, res) => {
 // app.post()
 // app.delete()
 
-module.exports = app
+module.exports = app;
---
+++ v2/controllers/logsController.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const logs = express.Router();
+const logsArray = require('models/logs.js')
+
+consolelog(logsArray)
+//parse incoming middleware
+app.use(express.json()) 
+
+
+// app.get('/', (req, res) => {
+//     res.send("Welcome to the Captain's Log, Ayee Matey 🪝⛵️")
+// })
+
+// app.get('/logs', (req, res) => {
+//     res.send(logs)
+// })
+
+// app.get('/logs/:arrayIndex', (req, res) => {
+//     const {arrayIndex} = req.params
+//     if(logs[arrayIndex]) {
+//         res.json(logs[arrayIndex])
+//     // console.log("excuse", logs[arrayIndex])
+//     } else 
+//     res.redirect( 'https://github.com/joinpursuit/captains-log-api' )
+// })
+
+// app.post('/logs', (req, res) => {
+//     const { captainName,
+//         title,
+//         post,
+//         mistakesWereMadeToday,
+//         daysSinceLastCrisis
+//     } = req.body;
+//     const newLog = req.body
+//     console.log(`sent, ${JSON.stringify(newLog)}`);
+//     logs.push(newLog)
+//     res.status(201).json(newLog)
+    
+// })
+
+// app.delete('/logs/:id', (req, res) => {
+//     const {id} = req.params
+//     logs.splice(id, 1)
+//     res.send('Ok')
+// })
+
+
+
+// const alphaOrder = (req, res, next) => {
+// console.log('✅')
+
+
+// if (req.body.hasOwnProperty('name')){
+//     return next();
+// } else {
+//     res.send('uh oh')
+// }
+
+// }
+
+// app.post('/', alphaOrder, (req, res) => {
+//     const sortedLog = Object.keys(logs).sort()
+//     console.log('sent')
+//     logs.push(sortedLog)
+//     res.send('Ok')
+// })
+
+// app.get('/logs?order=asc', (req, res) => {
+
+// })
+
+
+
+// app.all('*', (req, res) => { 
+//     res.status(404).send('<h1>404! Page not found</h1>'); 
+//   }); 
+
+//   app.use((req, res, next) => {
+//     console.log(req.method, req.headers.host, req.path)
+//     if (req.body.userName === 'Gary') {
+//     return next()
+// } else {
+// return res.status(403).send('no no no ')
+// }
+// })
+// app.post()
+// app.delete()
+
+module.export = logs
