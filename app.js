aa4eb41848421a3a878409bdc60fab70366a1c15
Kenneth-Parker <kennethkavon@gmail.com>
2023-10-22 00:55:10 -0400

    server up 404 all

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+    response.status(200).send("Hello, world!");
+});
+
+// 404 route
+app.use((req, res) => {
+    res.status(404).send("Not Found");
+});
+
+// app.all("*", (req, res) => {
+//     if (req.url === "/") {
+//         res.send("Hello, world!");
+//     } else {
+//         res.status(404).send("Not Found");
+//     }
+// });
+
+
+module.exports = app;
