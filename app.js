97cb3633c1732ebd6235b5fa3f8885db8568ec28
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-11-05 16:52:03 -0500

    cors added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const logs = require("./models/log.js");
 const app = express();
+const cors = require("cors")
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("welcome to the captains's log")
